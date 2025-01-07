985f25c09230f10ea5ba8ce5d003fe41fe8d821d
Hoai Nam <22010017@st.phenikaa-uni.edu.vn>
2025-01-07 21:40:09 +0700

    fix: Update image paths and improve static file serving

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const morgan = require('morgan')
 const { engine } = require('express-handlebars')
 const app = express()
 const port = 3000
-
+//static file
 app.use(express.static(path.join(__dirname, 'public')))
 
 //HTTP logger
@@ -15,7 +15,7 @@ app.engine('hbs', engine({
   extname: '.hbs'
 }));
 app.set('view engine', 'hbs')
-app.set('views', path.join(__dirname, 'resources\\views'))
+app.set('views', path.join(__dirname, 'resources', 'views'))
 
 app.get('/', (req, res) => {
   res.render('home')
