c6857eb204cde1983d64e823cb4cd20308462510
Hoai Nam <22010017@st.phenikaa-uni.edu.vn>
2025-01-08 21:13:05 +0700

    feat: Add store page with product listings and support sections

--- src/index.js
+++ src/index.js
@@ -21,6 +21,27 @@ app.get('/', (req, res) => {
   res.render('home')
 })
 
+app.get('/store', (req, res) => {
+  res.render('store', {
+      products: [
+          { name: 'Mac', image: '../img/macos.jpg' },
+          { name: 'iPhone', image: '../img/iphone15xanh.jpg' }
+      ],
+      newProducts: [
+          { name: 'iPhone 15 Pro', image: '../img/iphone15.jpg', description: 'Titan', price: '28.999.000đ' }
+      ],
+      support: [
+          { title: 'Chuyên gia', image: '../img/tv1.jpg', description: 'Hỗ trợ 24/7' }
+      ],
+      experiences: [
+          { title: 'Apple TV+', image: '../img/tv2.jpeg', description: 'Tặng 3 tháng miễn phí' }
+      ],
+      specialStores: [
+          { title: 'Giáo dục', image: '../img/tv3.jpg', description: 'Ưu đãi cho sinh viên' }
+      ]
+  });
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
